6950cbdec0b2caa704adff0179700228efba8b18
Yuri Yakymiv <yurithepro@gmail.com>
2018-07-12 19:38:55 -0400

    trying to figure out rnvi

--- app/navigation/main/Feed/index.js
+++ app/navigation/main/Feed/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { Icon } from 'native-base';
 
 class Feed extends Component {
+  static navigationOptions = {
+    tabBarIcon: ({ focused, tintColor }) => <Icon name="basketball" color={tintColor} />
+  }
+
   render() {
     return (
       <View />
